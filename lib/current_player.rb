2a1f18f053948e858d3ff0e49bb39bb9039d2996
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-12 23:35:05 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,15 +1,15 @@
 #current_player determines whose turn it is
 def current_player(board)
   if (turn_count(board) % 2 == 0)
-    return "O"
-  else
     return "X"
+  else
+    return "O"
   end
 end
 
 #turn_count will keep track of how many turns have been played
 def turn_count(board)
-  num_of_turns = 1
+  num_of_turns = 0
   board.each do |spot|
     if (spot == 'X' || spot == 'O')
       num_of_turns += 1
